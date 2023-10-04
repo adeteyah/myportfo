9bf5820aa74e3181328b0c3f4567eb816800be37
Adeteyah <adeteyah@gmail.com>
2023-10-04 11:42:24 +0700

    2%

--- script.js
+++ script.js
@@ -1,81 +1,109 @@
 // cursor
-// Create a GSAP timeline for the cursor
 const cursor = document.querySelector('.cursor');
 const cursorTimeline = gsap.timeline({ paused: true });
 
-// Initialize cursor position
 let mouseX = 0;
 let mouseY = 0;
 
-// Update cursor position on mousemove
-document.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+document.addEventListener('mousemove', e => {
+	mouseX = e.clientX;
+	mouseY = e.clientY;
 });
 
-// Update cursor position and play animation on mousemove
 document.addEventListener('mousemove', () => {
-  gsap.to(cursor, {
-      duration: 0.3, // Adjust the duration as needed
-      x: mouseX,
-      y: mouseY,
-      ease: 'power2.out', // Adjust easing function as needed
-      delay: 0.1 // Introduce a delay for the lag effect
-  });
+	gsap.to(cursor, {
+		duration: 0.3,
+		x: mouseX,
+		y: mouseY,
+		ease: 'power2.out',
+		delay: 0.1,
+	});
 });
 
-// Hide cursor when it's not moving
 document.addEventListener('mouseenter', () => {
-    cursor.style.opacity = 1;
+	cursor.style.opacity = 1;
 });
 
 document.addEventListener('mouseleave', () => {
-    cursor.style.opacity = 0;
+	cursor.style.opacity = 0;
 });
 
-// Add cursor hover effect on links
 const links = document.querySelectorAll('.brand');
 
-links.forEach((link) => {
-    link.addEventListener('mouseenter', () => {
-        gsap.to(cursor, { duration: 0.2, scale: 3 });
-    });
+links.forEach(link => {
+	link.addEventListener('mouseenter', () => {
+		gsap.to(cursor, { duration: 0.2, scale: 3 });
+	});
 
-    link.addEventListener('mouseleave', () => {
-        gsap.to(cursor, { duration: 0.2, scale: 1 });
-    });
+	link.addEventListener('mouseleave', () => {
+		gsap.to(cursor, { duration: 0.2, scale: 1 });
+	});
 });
-
 
 // home animations
 let tlHome = gsap.timeline({
-    scrollTrigger: {
-        trigger: '#home',
-        start: 'center',
-        end: 'bottom',
-        scrub: true,
-        markers: true,
-    }
-})
+	scrollTrigger: {
+		trigger: '#home',
+		start: 'center 20%',
+		end: 'bottom',
+		endTrigger: '#home',
+		scrub: true,
+	},
+});
 
 tlHome.to('.brand', {
-  y: -window.innerHeight
-})
+	y: -window.innerHeight,
+});
 
 // projects animations
+const projectElements = document.querySelectorAll('#projects .project');
+projectElements.forEach((project, index) => {
+	const tlProjects = gsap.timeline({
+		scrollTrigger: {
+			trigger: project,
+			start: 'top 80%',
+			end: 'bottom 20%',
+			scrub: true,
+			markers: true,
+			endTrigger: '#projects',
+		},
+	});
+
+	if (index % 2 === 0) {
+		tlProjects.from(project, {
+			opacity: 0,
+			x: -window.innerWidth,
+			ease: 'power2.out',
+		});
+	} else {
+		tlProjects.from(project, {
+			opacity: 0,
+			x: window.innerWidth,
+			ease: 'power2.out',
+		});
+	}
+	TweenMax.to('.project-img', 1, { autoAlpha: 0, ease: Elastic.easeInOut });
+	ScrollTrigger.create({
+		trigger: project,
+		animation: tlProjects,
+		start: 'top top',
+		end: 'bottom bottom',
+		scrub: true,
+	});
+});
 
 // contact animations
 
 // lenis
-const lenis = new Lenis()
+const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+lenis.on('scroll', e => {
+	console.log(e);
+});
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+	lenis.raf(time);
+	requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
